5449a61996a1901069124411f6a171048b2aab5c
Siddharth <siddharthkotkar@gmail.com>
2019-11-22 20:01:38 +0530

    first version

---
+++ src/main/java/com/nulab/task/cacoo/CacooApplication.java
@@ -0,0 +1,13 @@
+package com.nulab.task.cacoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacooApplication.class, args);
+	}
+
+}
